48f035580244b15c415630e9f547c56da9a73111
Endrit <endritramadanaj@hotmail.com>
2024-01-14 18:34:15 +0100

    RabbitMQ section

---
+++ src/AuctionService/Consumers/AuctionUpdatedConsumer.cs
@@ -0,0 +1,24 @@
+﻿using Contracts;
+using MassTransit;
+
+namespace AuctionService;
+
+public class AuctionUpdatedConsumer : IConsumer<Fault<AuctionUpdated>>
+{
+    public async Task Consume(ConsumeContext<Fault<AuctionUpdated>> context)
+    {
+        Console.WriteLine("--> Consuming faulty updating");
+
+        var exception = context.Message.Exceptions.First();
+
+        if (exception.ExceptionType == "System.ArgumentException")
+        {
+            context.Message.Message.Model = "FooBar";
+            await context.Publish(context.Message.Message);
+        }
+        else 
+        {
+            Console.WriteLine("Not an argument exception - update error dashboard somewhere");
+        }
+    }
+}
--- src/AuctionService/RequestHelpers/MappingProfiles.cs
+++ src/AuctionService/RequestHelpers/MappingProfiles.cs
@@ -1,6 +1,7 @@
 ﻿using AuctionService.DTOs;
 using AuctionService.Entities;
 using AutoMapper;
+using Contracts;
 
 namespace AuctionService.RequestHelpers;
 
@@ -13,5 +14,8 @@ public class MappingProfiles : Profile
         CreateMap<CreateAuctionDTO, Auction>()
             .ForMember(d => d.Item, o => o.MapFrom(s => s));
         CreateMap<CreateAuctionDTO, Item>();
+        CreateMap<AuctionDTO, AuctionCreated>();
+        CreateMap<Auction, AuctionUpdated>().IncludeMembers(a => a.Item);
+        CreateMap<Item, AuctionUpdated>();
     }
 }
