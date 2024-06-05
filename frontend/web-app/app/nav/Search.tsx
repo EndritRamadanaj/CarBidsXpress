90f437d263f56d46a0c71983266fc9c013e3c1aa
Endrit <endritramadanaj@hotmail.com>
2024-06-05 10:34:09 +0200

    CRUD operations in client app

--- frontend/web-app/app/nav/Search.tsx
+++ frontend/web-app/app/nav/Search.tsx
@@ -1,10 +1,13 @@
 'use client'
 
 import { useParamsStore } from '@/hooks/useParamsStore';
+import { usePathname, useRouter } from 'next/navigation';
 import React, { useState } from 'react'
 import { FaSearch } from 'react-icons/fa';
 
 export default function Search() {
+    const router = useRouter();
+    const pathName = usePathname();
     const setParams = useParamsStore(state => state.setParams);
     const setSearchValue = useParamsStore(state => state.setSearchValue);
     const searchValue = useParamsStore(state => state.searchValue);
@@ -14,7 +17,8 @@ export default function Search() {
     }
 
     function search() {
-        setParams({searchTerm: searchValue});
+        if (pathName !== '/') router.push('/');
+        setParams({ searchTerm: searchValue });
     }
 
     return (
